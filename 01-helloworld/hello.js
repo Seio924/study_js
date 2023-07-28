7ee3b01db0ee9dc2329559e49a9f052548ef7510
Seio924 <seio9241@gmail.com>
2023-07-28 15:01:07 +0900

    test

--- 01-helloworld/hello.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", helloMbti);
-
-function helloMbti() {
-  const heading = document.querySelector("h1");
-  const mbti = prompt("당신의 mbti는?");
-  heading.append("헬로" + mbti);
-}
